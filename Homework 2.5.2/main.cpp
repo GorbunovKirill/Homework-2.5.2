7e10d7a8fc00772e05a83981d90663aad32b5fec
Kirill Gorbunov <4pduck@gmail.com>
2023-05-03 15:35:13 +0300

    again fixes commit

--- Homework 2.5.2/main.cpp
+++ Homework 2.5.2/main.cpp
@@ -66,8 +66,12 @@ class triangle: public figure {
 
 public:
 
-	triangle(int a, int b, int c, int  A, int B, int C) {
-	this->a = a; this->b = b; this->c = c; this->A = A; this->B = B; this->C = C;  name = "Треугольник: "; }
+	triangle(int a, int b, int c, int  A, int B, int C){
+
+		this->a = a; this->b = b, this->c = c; 
+		this->A = A; this->B = B, this->C = C;
+
+	  name = "Треугольник: "; }
 
 	void print_info() override {
 		std::cout << get_name() << std::endl;
@@ -83,84 +87,61 @@ class quadrangle:public figure {
 
 public:
 
-	quadrangle(int a, int b, int c, int d, int  A, int B, int C, int D) {
-	this->a = a; this->b = b; this->c = c; this->d = d; this->A = A; this->B = B; this->C = C; this->D = D; name = "Четырёхугольник: "; }
+	quadrangle(int a, int b, int c, int d, int  A, int B, int C, int D){
 
-	int get_a() {
-		return a;
-	}
-	int get_b() {
-		return b;
-	}
-	int get_c() {
-		return c;
-	}
-	int get_d() {
-		return d;
-	}
-	int get_A() {
-		return A;
-	}
-	int get_B() {
-		return B;
-	}
-	int get_C() {
-		return C;
-	}
-	int get_D() {
-		return D;
-	}
-	std::string get_name() {
-		return name;
-	}
+		this->a = a; this->b = b, this->c = c; this->d = d;
+		this->A = A; this->B = B, this->C = C; this->D = D;
+	 name = "Четырёхугольник: "; }
+
+
 };
 
 class right_tri :public triangle {
 public:
 	right_tri(int a, int b, int c, int  A, int B) :triangle(a,b,c,A,B,90){
-	this->a = a; this->b = b; this->c = c; this->A = A; this->B = B;  name = "Прямоугольный треугольник: ";
+	name = "Прямоугольный треугольник: ";
 	}
 };
 
 class isos_tri :public triangle {
 public:
 isos_tri(int a, int b, int  A, int B) :triangle(a, b, a, A, B, A) {
-		this->a = a; this->b = b; this->A =A; this->B = B;  name = "Равнобедренный треугольник: ";
+	 name = "Равнобедренный треугольник: ";
 	}
 };
 
 class equil_tri :public triangle {
 public:
 	equil_tri(int a) :triangle(a, a, a, 60, 60, 60) {
-		this->a =  a;  name = "Равносторонний треугольник: ";
+	 name = "Равносторонний треугольник: ";
 	}
 };
 
 class rectangle :public quadrangle {
 public:
 	rectangle(int a,int b) :quadrangle(a, b, a,b, 90, 90, 90, 90) {
-		this->a =  a; this->b = b;  name = "Прямоугольник: ";
+		 name = "Прямоугольник: ";
 	}
 };
 
 class square :public quadrangle {
 public:
 	square(int a) :quadrangle(a, a, a, a, 90, 90, 90, 90) {
-		this->a =  a;   name = "Квадрат: ";
+	 name = "Квадрат: ";
 	}
 };
 
 class parallelogram :public quadrangle {
 public:
 	parallelogram(int a, int b,int A,int B) :quadrangle(a, b, a, b, A, B, A, B) {
-		this->a = a; this->b = b; this->A =  A; this->B = B;  name = "Параллелограмм: ";
+		name = "Параллелограмм: ";
 	}
 };
 
 class rhomb :public quadrangle {
 public:
 	rhomb(int a, int A, int B) :quadrangle(a, a, a, a, A, B, A, B) {
-		this->a = a; this->A = A; this->B =  B;  name = "Ромб: ";
+		 name = "Ромб: ";
 	}
 };
 
@@ -180,15 +161,32 @@ int main() {
 	rhomb ninth(40, 30, 120);
 
 	
-	first.print_info();
-	second.print_info();
-	third.print_info();
-	fourth.print_info();
-	fith.print_info();
-	sixth.print_info();
-	seventh.print_info();
-	eighth.print_info();
-	ninth.print_info();
+	figure* one = &first;
+	one->print_info();
+
+	figure* two = &second;
+	two->print_info();
+
+	figure* three = &third;
+	three->print_info();
+
+	figure* four = &fourth;
+	four->print_info();
+
+	figure* five = &fith;
+	five->print_info();
+
+	figure* six = &sixth;
+	six->print_info();
+
+	figure* seven = &seventh;
+	seven->print_info();
+
+	figure* eight = &eighth;
+	eight->print_info();
+
+	figure* nine = &ninth;
+	nine->print_info();
 
 
 	return 0;
